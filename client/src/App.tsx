7b3b2d529dbad158fe8453b2cb66e768b610ebae
Rudresh Srivastava <srivastavarudresh552@gmail.com>
2026-07-06 18:05:08 +0530

    feat: Add Layout, Sidebar, AIComposer, Accounts, Dashboard, and Scheduler components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                {/* 15:54 */}
             </Routes>
         </>
     );
