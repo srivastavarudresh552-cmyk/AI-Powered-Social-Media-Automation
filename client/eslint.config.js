6b13a36dfe0533699942c66247083a4fab1e9c61
Rudresh Srivastava <srivastavarudresh552@gmail.com>
2026-09-05 10:47:46 +0530

    Completed post scheduler page UI

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -21,7 +21,7 @@ export default defineConfig([
     rules: {
       // Note: you must disable the base rule as it can report incorrect errors
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ]);
